a4d12ea845a2e41b764bfe08900f36c791623e28
Jason Mitchell <jason.mitchell.w@gmail.com>
2016-06-29 23:17:28 -0700

    Adding support to run ember serve with ember share

--- index.js
+++ index.js
@@ -4,29 +4,30 @@
 var ncp = require('copy-paste');
 var ngrok = require('ngrok');
 var Promise = require('ember-cli/lib/ext/promise');
+var ServeCommand = require('ember-cli/lib/commands/serve');
 
 module.exports = {
   name: 'ember-share',
   includedCommands: function() {
     return {
-      share: {
+      share: ServeCommand.extend({
         name: 'share',
         description: 'Share your local Ember apps with the world using ngrok.',
-        availableOptions: [
-          { name: 'port', type: Number, default: 4200, aliases: ['p'] }
-        ],
+
         run: function(commandOptions, rawArgs) {
           var port = commandOptions.port;
           var self = this;
 
-          return new Promise(function(resolve, reject) {
+          var ngrokServer = new Promise(function(resolve, reject) {
             ngrok.connect(port, function(err, url) {
               ncp.copy(url);
               self.ui.writeLine('Your sharable URL is ' + url + ' and has been copied to your clipboard!');
             });
           });
+
+          return Promise.all([ngrokServer, this._super.run.apply(this, arguments)]);
         }
-      }
+      })
     }
   }
 };
