0a37af2845a44b38038143119217e6c002242ae3
David Tang <david.tang@verizondigitalmedia.com>
2017-08-17 01:34:26 -0700

    revert ES6 in index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /* eslint-env node */
 'use strict';
 
-let ncp = require('copy-paste');
-let ngrok = require('ngrok');
-let Promise = require('rsvp').Promise;
-let ServeCommand = require('ember-cli/lib/commands/serve');
+var ncp = require('copy-paste');
+var ngrok = require('ngrok');
+var Promise = require('rsvp').Promise;
+var ServeCommand = require('ember-cli/lib/commands/serve');
 
 module.exports = {
   name: 'ember-share',
@@ -15,13 +15,13 @@ module.exports = {
         description: 'Share your local Ember apps with the world using ngrok.',
 
         run: function(commandOptions, rawArgs) {
-          let port = commandOptions.port;
-          let self = this;
+          var port = commandOptions.port;
+          var self = this;
 
-          let ngrokServer = new Promise(function(resolve, reject) {
+          var ngrokServer = new Promise(function(resolve, reject) {
             ngrok.connect(port, function(err, url) {
               ncp.copy(url);
-              self.ui.writeLine(`Your sharable URL is ${url} and has been copied to your clipboard.`);
+              self.ui.writeLine('Your sharable URL is ' + url + ' and has been copied to your clipboard.');
             });
           });
 
