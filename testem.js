fbebb6d8faebb89fc3bc868dbae1764d554319ac
David Tang <david.tang@verizondigitalmedia.com>
2017-08-17 00:59:28 -0700

    upgrade ember cli to 2.14.2

--- testem.js
+++ testem.js
@@ -1,7 +1,6 @@
 /*jshint node:true*/
 module.exports = {
-  "framework": "qunit",
-  "test_page": "tests/index.html?hidepassed",
+  test_page: "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
     "PhantomJS"
